9242ce9d5862edb83c13ea0b1d4d1c61ab45e1dd
xXG4briel <gabrielgeorge95@gmail.com>
2022-12-27 20:26:59 -0300

    projects route e sqlite3

--- models/project.js
+++
@@ -1,8 +0,0 @@
-async function getProjects(){
-}
-async function setProject(body){
-}
-async function updateProject(id, body){
-}
-async function deleteProject(id){
-}
--- routes/projects.js
+++ routes/projects.js
@@ -1,49 +1,88 @@
 const express = require('express');
 const router = express.Router();
-const Projects = require('../models/project')
+const sqlite3 = require('sqlite3');
 
-// C -> Create
-router.post('/', async (req, res) => {
-    const body = JSON.parse(req.body);
-    try {
-        const projects = await Projects.setProject(body);
-        res.status(200).json(projects);
-    } catch (error) {
-        res.status(400).json({ message: error.message })
+let db = new sqlite3.Database('portfolio.db', (err) => {
+    if (err) {
+      console.error("[x] getting error " + err);
+    }
+    else {
+      console.log('[+] connected to the database.')
+      db.exec(`CREATE TABLE IF NOT EXISTS projects (
+        projectId INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        title TEXT NOT NULL,
+        url TEXT NOT NULL,
+        description TEXT NOT NULL,
+        image TEXT NOT NULL
+      );`, (error) => {  if(error) console.error(error) });
     }
 });
-// R -> Read
-router.get('/', async (req, res) => {
+
+router.post('/', (req, res) => {
     try {
-        const projects = await Projects.getProjects();
-        res.status(200).json(projects);
+        const body = req.body;
+        const values = [body.name, body.title, body.url, body.description, body.image];
+        const sql = `INSERT INTO projects (name, title, url, description, image)
+        VALUES (?, ?, ?, ?, ?)`;
+        db.run(sql, values, ( error ) => {
+                if (error) {
+                    return res.status(400).json({ message: error.message });
+                }
+                res.status(200).json({ message: "Ok" });
+            }
+        );
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+router.get('/', (req, res) => {
+    try {
+        const sql = `SELECT * FROM projects;`;
+        db.all(sql, (error, rows) => {
+                if (error) {
+                    return res.status(400).json({ message: error.message });
+                }
+                res.status(200).json(rows);
+            }
+        );
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 });
-// U -> Update
-router.put('/:id', async (req, res) => {
+router.put('/:id', (req, res) => {
     const { id } = req.params;
-    const body = JSON.parse(req.body);
+    const body = req.body;
+    const values = [body.name, body.title, body.url, body.description, body.image, id];
+    const sql = `UPDATE projects
+    SET name = ?,
+    title = ?,
+    url = ?,
+    description = ?,
+    image = ?
+    WHERE projectId = ?`;
     try {
-        const projects = await Projects.updateProject(id, body);
-        if (projects) {
-            res.status(200).json(projects);
-        }
-        res.status(400).json({ message: "Não foi possível criar o projeto." });
+        db.run(sql, values, (error) => {
+            if (error) {
+                return res.status(400).json({ message: error.message });
+            }
+            res.status(200).json({ message: "Ok" });
+        });
     }
     catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
-// D -> Delete
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', (req, res) => {
     const { id } = req.params;
+    const sql = `DELETE FROM projects WHERE projectId = ?`;
     try {
-        if(await Projects.deleteProject(id)) {
-            res.status(200).json("Ok");
-        }
-        res.status(400).json("Não foi possível apagar o projeto.");
+        db.run(sql, id, (error) => {
+            if (error) {
+                return res.status(400).json({ message: error.message });
+            }
+            res.status(200).json({ message: "Ok" });
+        });
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
